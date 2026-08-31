433651467e3b12b87633c799f8445179c7e62a9f
Alex <alejandro_infinito@outlook.com>
2026-08-31 11:06:10 -0600

    fix: update Dockerfile paths and move to root directory

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,13 @@
 FROM mcr.microsoft.com/dotnet/sdk:8.0 AS build
 WORKDIR /src
-COPY ["PruebaMRP.csproj", "./"]
-RUN dotnet restore "PruebaMRP.csproj"
-COPY . .
+COPY ["src/Api/PruebaMRP.csproj", "Api/"]
+RUN dotnet restore "Api/PruebaMRP.csproj"
+COPY src/Api/ Api/
+WORKDIR "/src/Api"
+RUN dotnet build "PruebaMRP.csproj" -c Release -o /app/build
 RUN dotnet publish "PruebaMRP.csproj" -c Release -o /app/publish /p:UseAppHost=false
 
 FROM mcr.microsoft.com/dotnet/aspnet:8.0 AS final
 WORKDIR /app
-EXPOSE 8080
-ENV ASPNETCORE_URLS=http://+:8080
 COPY --from=build /app/publish .
 ENTRYPOINT ["dotnet", "PruebaMRP.dll"]
--- src/Api/obj/Debug/net8.0/PruebaMRP.AssemblyInfo.cs
+++ src/Api/obj/Debug/net8.0/PruebaMRP.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PruebaMRP")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5522592144f0b62b50f10586fdb7a8d137d4323d")]
 [assembly: System.Reflection.AssemblyProductAttribute("PruebaMRP")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PruebaMRP")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
